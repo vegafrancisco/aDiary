3fd126d983cad675f2ff70299ea2379c82cf9ed5
AAyres04 <30842859+AAyres04@users.noreply.github.com>
2018-04-17 18:07:55 -0300

    Se agrega la clase mision.
    Esta  junto a una sugerencia para proceder, y su respectiva documentacion para utilizar sus componentes.
    Si se quiere editar el codigo ya sea, agregar o quitar algo, crear un "branch" para que despues todos lo revisemos antes de fusionar con master. Esto aplica para todo lo que hagamos.

---
+++ Mision.java
@@ -0,0 +1,97 @@
+/*
+ * Proyecto aDiary
+ * Alfredo Mendez, Antonio Ayres, Maximiliano Torres, Francisco Vega
+ */
+package adiary;
+
+/**
+ * Esta clase permite guardar tanto la mision(tarea) en si,
+ * como el estado de esta (completa o incompleta).
+ * @author AAyres04
+ */
+
+public class Mision {
+    
+    private String mision; //La mision(tarea).
+    private boolean isCompleted; //Indica el estado de la tarea.
+    
+    /**
+    * Constructor sin parametros, con valores por default.
+    * atributo mision = ""
+    * atributo isCompleted = false.
+    */
+    
+    public Mision(){
+        this.mision = "";
+        this.isCompleted = false;
+    }
+    
+    /**
+     * Constructor con parametros para inicializarlo con valores otorgados.
+     * @param mision - String
+     * @param isCompleted - boolean.
+    */
+    
+    public Mision(String mision, boolean isCompleted){
+        this.mision = mision;
+        this.isCompleted = isCompleted;
+    }
+    
+    /**
+     * Asigna una nueva mision a la insiancia de esta clase.
+     * @param mision 
+     */
+
+    public void setMision(String mision) {
+        this.mision = mision;
+    }
+    
+    /**
+     * Asigna un nuevo valor al booleano isCompleted a a la insiancia de esta clase.
+     * @param isCompleted - boolean, representa el estado de la mision.
+     */
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+    
+    /**
+     * Obtiene el valor de "mision" de la instancia de esta clase.
+     * @return String.
+     */
+
+    public String getMision() {
+        return mision;
+    }
+    
+    /**
+     * Obtiene el valor de "isCompleted" de la instancia de esta clase.
+     * @return boolean.
+     */
+
+    public boolean getIsCompleted() {
+        return isCompleted;
+    }
+    
+    /**
+     * Traduce el valor del booleano "isCompleted" al estado que este representa.
+     * @return String.
+     */
+    
+    public String estado(){
+        if(this.isCompleted) return "Achieved";
+        else return "Incompleto";
+    }
+    
+    /**
+     * Override de toString(). 
+     * Devuelve información sobre la mision y su estado.
+     * @return String
+     */
+    
+    @Override
+    public String toString() {
+        return "Mision: " + this.mision + "\t" +  "Estado" + estado() + '}';
+    }
+    
+}
